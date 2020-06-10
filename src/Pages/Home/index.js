ad8d8cfedd77026eddf388208c2e9a140d83ee2b
Silvio Júnior <silvio_junior96@hotmail.com>
2020-06-10 11:48:12 -0300

    Melhorias de design

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,5 +1,7 @@
-import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, TextInput } from 'react-native';
+import React, { useEffect, useState } from 'react';
+//import { View, Text, StyleSheet, TouchableOpacity, TextInput } from 'react-native';
+import { Text, TextInput, View, StyleSheet, LayoutAnimation, Platform, UIManager, TouchableOpacity, Image } from 'react-native';
+
 import Icon from 'react-native-vector-icons/FontAwesome5';
 
 //Cores do IBTI:
@@ -9,6 +11,38 @@ import Icon from 'react-native-vector-icons/FontAwesome5';
 // #109F6C  VERDE CLARO
 
 export default function Home() {
+    //Inicialização das variáveis
+    const [expanded, setExpanded] = useState(false); //Controla o estado dos detalhes (expanded, collapsed)
+    const [frequencyCounter, setFrequencyCounter] = useState(0);
+
+    //Para funcionar no Android sem bugs, no iOS já funciona corretamente
+    if (Platform.OS === 'android') {
+        UIManager.setLayoutAnimationEnabledExperimental(true);
+    }
+
+    //Função que realiza a troca do estado dos detalhes do dispositivo, bem como tipo de animação realizada na troca de estado
+    function changeLayout() {
+        LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+        let expand = !expanded; //Pegamos o oposto do valor atual de expanded, para abrir se estiver fechada, e fechar se estiver aberta
+        setExpanded(expand); //Passamos o novo valor para a expanded
+    }
+
+    //Função que aumenta a frequência
+    function moreFrequency() {
+        if ((frequencyCounter + 1) < 60) {
+            let freq = frequencyCounter + 1;
+            setFrequencyCounter(freq);
+        }
+    }
+
+    //Função que diminui a frequência
+    function lessFrequency() {
+        if ((frequencyCounter - 1) > -1) {
+            let freq = frequencyCounter - 1;
+            setFrequencyCounter(freq);
+        }
+    }
+
     return (
         <View style={{ flex: 1, heigh: '100%' }}>
             <View style={styles.difArea}>
@@ -16,31 +50,42 @@ export default function Home() {
             </View>
 
             <View style={styles.devicesArea}>
-                <View style={styles.device}>
+                <TouchableOpacity style={styles.device} onPress={() => { changeLayout() }}>
+                    <Icon name="chevron-down" color={'#FFF'} size={20}></Icon>
                     <Text style={styles.deviceText}>Dispositivo 1</Text>
-                </View>
-
-                <View style={styles.device}>
-                    <Text style={styles.deviceText}>Dispositivo 2</Text>
-                </View>
-            </View>
-
-            <View style={styles.btnArea}>
-                <TouchableOpacity style={styles.btnBuzzer}>
-                    <Text style={styles.textBuzzer}>Buzzer</Text>
                 </TouchableOpacity>
+                <View style={{ height: expanded ? null : 0, overflow: 'hidden' }}>
+                    <View style={styles.deviceDetailsArea}>
+                        <Text>GRÁFICO</Text>
+                        <Image style={{ width: 300, height: 200 }} source={require('../../assets/images/lineGraphic.png')}></Image>
+                        <Text>Última Temperatura Registarda: 20°</Text>
+                        <Text>AQUI VÃO APARECER OS DADOS DO DISPOSITIVO 1</Text>
+                        <Text>AQUI VÃO APARECER OS DADOS DO DISPOSITIVO 1</Text>
+                    </View>
 
-                <View style={styles.frequencyChange}>
-                    <TouchableOpacity style={styles.btnLessFrequency}>
-                        <Icon name="minus"></Icon>
-                    </TouchableOpacity>
+                    <View style={styles.btnArea}>
+                        <TouchableOpacity style={styles.btnBuzzer}>
+                            <Icon name="bullhorn" color={'#FFF'} size={25}></Icon>
+                        </TouchableOpacity>
 
-                    <TextInput style={styles.frequencyInput}></TextInput>
+                        <View style={styles.frequencyChange}>
+                            <TouchableOpacity style={styles.btnLessFrequency} onPress={() => { lessFrequency() }}>
+                                <Icon name="minus" color={'#FFF'} size={15}></Icon>
+                            </TouchableOpacity>
 
-                    <TouchableOpacity style={styles.btnMoreFrequency}>
-                        <Icon name="plus"></Icon>
-                    </TouchableOpacity>
+                            <Text style={styles.frequencyInput}>{frequencyCounter}</Text>
+
+                            <TouchableOpacity style={styles.btnMoreFrequency} onPress={() => { moreFrequency() }}>
+                                <Icon name="plus" color={'#FFF'} size={15}></Icon>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
                 </View>
+
+                <TouchableOpacity style={styles.device}>
+                    <Icon name="chevron-down" color={'#FFF'} size={20}></Icon>
+                    <Text style={styles.deviceText}>Dispositivo 2</Text>
+                </TouchableOpacity>
             </View>
 
         </View>
@@ -68,6 +113,7 @@ const styles = StyleSheet.create({
         justifyContent: "flex-start"
     },
     device: {
+        flexDirection: "row",
         marginTop: 10,
         padding: 15,
         width: '95%',
@@ -76,7 +122,14 @@ const styles = StyleSheet.create({
         borderRadius: 5
     },
     deviceText: {
-        color: '#FFF'
+        color: '#FFF',
+        marginLeft: 5
+    },
+    deviceDetails: {
+
+    },
+    deviceDetailsArea: {
+        height: '50%'
     },
     btnArea: {
         flexDirection: "row",
@@ -85,6 +138,10 @@ const styles = StyleSheet.create({
         justifyContent: "flex-end"
     },
     btnBuzzer: {
+        width: 50,
+        height: 50,
+        alignItems: "center",
+        justifyContent: "center",
         marginRight: 15,
         padding: 10,
         backgroundColor: '#0B7534',
@@ -99,6 +156,9 @@ const styles = StyleSheet.create({
         marginRight: 15
     },
     frequencyInput: {
+        fontSize: 15,
+        textAlign: "center",
+        textAlignVertical: "center",
         width: 50,
         height: 50,
         borderWidth: 1,
@@ -112,7 +172,8 @@ const styles = StyleSheet.create({
         padding: 5,
         borderWidth: 1,
         borderLeftWidth: 0,
-        borderColor: '#999'
+        borderColor: '#0B7534',
+        backgroundColor: '#0B7534'
     },
     btnLessFrequency: {
         width: 50,
@@ -122,7 +183,8 @@ const styles = StyleSheet.create({
         padding: 5,
         borderWidth: 1,
         borderRightWidth: 0,
-        borderColor: '#999'
+        borderColor: '#D9534F',
+        backgroundColor: '#D9534F'
     }
 
 })
